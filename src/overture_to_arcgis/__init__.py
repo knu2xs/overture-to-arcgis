e43a4a6f0e90438e8f160912bba086a00a480f7d
Joel McCune <knu2xs@gmail.com>
2026-02-26 10:17:53 -0800

    Rename __main__.py to _main.py and update import\n\nRename the main module from __main__.py to _main.py to better reflect\nits role as a private module rather than an entry point. Update the\nimport in __init__.py accordingly.

--- src/overture_to_arcgis/__init__.py
+++ src/overture_to_arcgis/__init__.py
@@ -4,7 +4,7 @@ __author__ = "Joel McCune (https://github.com/knu2xs)"
 __license__ = "Apache 2.0"
 __copyright__ = "Copyright 2025 by Joel McCune (https://github.com/knu2xs)"
 
-from .__main__ import get_spatially_enabled_dataframe, get_features
+from ._main import get_spatially_enabled_dataframe, get_features
 from . import utils
 
 __all__ = ["get_spatially_enabled_dataframe", "get_features", "utils"]
